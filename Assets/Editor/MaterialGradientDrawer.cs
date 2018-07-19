7540a8e0d5abb48a2e94bd5a11c1d3be82588e8e
Liam Hurt <liamhurt51@gmail.com>
2018-07-19 13:20:06 -0700

    Fixing errors within the gradient drawer and adding the file for the editor window

--- Assets/Editor/MaterialGradientDrawer.cs
+++ Assets/Editor/MaterialGradientDrawer.cs
@@ -12,8 +12,8 @@ public class MaterialGradientDrawer : PropertyDrawer {
     {
         Event guiEvent = Event.current;
         MaterialGradient grad = (MaterialGradient)fieldInfo.GetValue(prop.serializedObject.targetObject);
-        float labelWidth = guiEvent.skin.label.CalcSize(label).x + 5;
-        Rect textRect = new Rect(pos.x, +labelWidth, pos.y, pos.width - labelWidth, pos.height);
+        float labelWidth = GUI.skin.label.CalcSize(label).x + 5;
+        Rect textRect = new Rect(pos.x + labelWidth, pos.y, pos.width - labelWidth, pos.height);
 
         // if (!mapPrev) mapPrev = GameObject.Find("MapPreview").GetComponent<MapPreview>();
 
@@ -23,7 +23,7 @@ public class MaterialGradientDrawer : PropertyDrawer {
 
             GUI.Label(pos, label);
             gradStyle.normal.background = grad.GetTexture((int)pos.width);
-            guiEvent.Label(textRect, GUIContent.none, gradStyle);
+            GUI.Label(textRect, GUIContent.none, gradStyle);
 
             // if (mapPrev && mapPrev.autoUpdate) mapPrev.DrawMapInEditorGrad();
         }
